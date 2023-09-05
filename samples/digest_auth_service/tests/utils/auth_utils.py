b91b8da7851af97b0b27ace31871bc6e537c4faf
Niyaz <nmingaripov@gmail.com>
2023-09-05 14:58:06 +0000

    fix: got rid of HTTPDigestAuth lib

--- samples/digest_auth_service/tests/utils/auth_utils.py
+++ samples/digest_auth_service/tests/utils/auth_utils.py
@@ -1,5 +1,6 @@
-from requests.auth import HTTPDigestAuth
 import re
+import hashlib
+import time
 # parsing regex
 reg = re.compile(r'(\w+)[:=][\s"]?([^",]+)"?')
 
@@ -11,13 +12,9 @@ def construct_challenge(auth_directives: dict, nonce=''):
             'qop': "auth"
             }
 
-
 # construct authorization header sent from client
 def construct_header(username: str, password: str, challenge: dict):
-    digest_auth = HTTPDigestAuth(username, password)
-    digest_auth.init_per_thread_state()
-    digest_auth._thread_local.chal = challenge
-    return digest_auth.build_digest_header('GET', '/v1/hello')
+    return build_digest_header('GET', '/v1/hello', challenge, username, password)
 
 
 def parse_directives(authentication_header: str):
@@ -29,3 +26,66 @@ def auth_directives_assert(auth_directives: dir):
     assert 'nonce' in auth_directives
     assert 'algorithm' in auth_directives
     assert 'qop' in auth_directives
+
+# construct authorization header sent from client
+def build_digest_header(method, uri, chal, username, password):
+        realm = chal['realm']
+        nonce = chal['nonce']
+        qop = chal.get('qop')
+        algorithm = chal.get('algorithm')
+        opaque = chal.get('opaque')
+        hash_utf8 = None
+
+        if algorithm is None:
+            _algorithm = 'MD5'
+        else:
+            _algorithm = algorithm.upper()
+        if _algorithm == 'MD5':
+            def md5_utf8(x):
+                x = x.encode('utf-8')
+                return hashlib.md5(x).hexdigest()
+            hash_utf8 = md5_utf8
+        elif _algorithm == 'SHA256':
+            def sha256_utf8(x):
+                x = x.encode('utf-8')
+                return hashlib.sha256(x).hexdigest()
+            hash_utf8 = sha256_utf8
+        elif _algorithm == 'SHA512':
+            def sha512_utf8(x):
+                x = x.encode('utf-8')
+                return hashlib.sha512(x).hexdigest()
+            hash_utf8 = sha512_utf8
+        else:
+            return None
+
+        KD = lambda s, d: hash_utf8("%s:%s" % (s, d))
+
+        A1 = '%s:%s:%s' % (username, realm, password)
+        A2 = '%s:%s' % (method, uri)
+
+        HA1 = hash_utf8(A1)
+        HA2 = hash_utf8(A2)
+
+        nonce_count = 1
+        ncvalue = '%08x' % nonce_count
+        s = str(nonce_count).encode('utf-8')
+        s += nonce.encode('utf-8')
+        s += time.ctime().encode('utf-8')
+
+        cnonce = (hashlib.sha1(s).hexdigest()[:16])
+
+        noncebit = "%s:%s:%s:%s:%s" % (
+                nonce, ncvalue, cnonce, 'auth', HA2
+        )
+        response_digest = KD(HA1, noncebit)
+            
+        base = 'username="%s", realm="%s", nonce="%s", uri="%s", ' \
+               'response="%s"' % (username, realm, nonce, uri, response_digest)
+        if opaque:
+            base += ', opaque="%s"' % opaque
+        if algorithm:
+            base += ', algorithm="%s"' % algorithm
+        if qop:
+            base += ', qop="auth", nc=%s, cnonce="%s"' % (ncvalue, cnonce)
+
+        return 'Digest %s' % (base)
